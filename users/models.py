80b4f97e6dac4f44c36c8550f8dabd0b43686e13
Cyber-Cowboy <OlegKorkeshkoV@yandex.ru>
2020-07-25 12:34:26 +0500

    profile added

--- users/models.py
+++ users/models.py
@@ -3,7 +3,9 @@ from django.conf import settings
 
 class Profile(models.Model):
 	user = models.OneToOneField(settings.AUTH_USER_MODEL,
-								on_delete=models.CASCADE)
+								on_delete=models.CASCADE,
+								related_name="profile")
+	avatar = models.CharField(max_length=500, blank=True)
 	def __str__(self):
 		return f'Profile for user: {self.user.username}'
 
--- users/views.py
+++ users/views.py
@@ -5,7 +5,10 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.urls import reverse
 from django.db.utils import IntegrityError
+from django.views import generic
 from .forms import LoginForm, UserRegistrationForm
+from .models import Profile
+from anime_titles.models import TitleInList, AnimeList
 
 def register_view(request):
 	template = "users/register.html"
@@ -20,6 +23,8 @@ def register_view(request):
 			new_user.set_password(
 				user_form.cleaned_data['password'])
 			new_user.save()
+			Profile(user=new_user).save()
+			AnimeList(user=new_user).save()
 			return HttpResponseRedirect(reverse("users:login"))
 	return render(request, template,
 				{"form":user_form})
@@ -49,3 +54,12 @@ def login_view(request):
 def logout_view(request):
 	logout(request)
 	return HttpResponseRedirect(reverse("anime_titles:index"))
+
+class ProfileView(generic.DetailView):
+	template_name = "users/profile.html"
+	model = Profile
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		profile = Profile.objects.get(pk=self.kwargs['pk'])
+		context['titles'] = TitleInList.objects.filter(anime_list=profile.user.animelist)
+		return context
